324deb100eb36c3613da4a9efa36600e3c15a332
William <william.porto@nave.rs>
2022-11-15 12:20:29 -0300

    setup tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
+import "../styles/global.css";
+
 interface LayoutProps {
-  readonly children?: React.ReactNode
+  readonly children?: React.ReactNode;
 }
 
-export default function Layout({
-  children,
-}: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
