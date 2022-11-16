850fe2d35d8dee32bb4af6a6861d5caba9a30aa3
William <william.porto@nave.rs>
2022-11-15 21:20:37 -0300

    changing default page title

---
+++ app/meta.ts
@@ -0,0 +1,4 @@
+export default {
+  title: "definitely-not-next-template",
+  description: "definitely not a next template",
+};
--- app/page.e2e.ts
+++ app/page.e2e.ts
@@ -1,12 +1,14 @@
 import { test, expect } from "@playwright/test";
 
+import meta from "@app/meta";
+
 test("if homepage is being rendered correctly", async ({ page }) => {
   await page.goto("/");
 
   const text = page.getByText("Page");
 
   await Promise.all([
-    expect(page).toHaveTitle("Next Auth Tailwindcss"),
+    expect(page).toHaveTitle(meta.title),
     expect(text).toBeVisible(),
   ]);
 });
